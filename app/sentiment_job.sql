115da47c88c075b56270c4b81c6a4eb3dab963d2
OwenLoza <owen.loza@iteso.mx>
2026-03-17 19:25:29 -0600

    Otro cambio más Ant

--- app/sentiment_job.sql
+++ app/sentiment_job.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE user_sentiment_metrics AS
 WITH  user_sentiment AS (
     select author_id, content, SNOWFLAKE.CORTEX.COMPLETE(
-        'mistral-large2',
+        'mistral-7b',
         CONCAT('Respond with exactly "TRUE" if the comment is overall positive, and "FALSE" otherwise: ', content)
     ) as sentiment_metric from posts
 )
